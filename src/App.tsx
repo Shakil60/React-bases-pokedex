22cfc924be3f01a1c1ea8c5ae43455a7799190c3
Shakil60 <khaldishakil@gmail.com>
2026-09-04 15:13:04 +0200

    prop pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import './App.css'
-import PokemonCard, { pokemonList } from './components/PokemonCard';
+import PokemonCard from './components/PokemonCard';
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+    {
+    name: "mew",
+  },
+];
 
 function App() {
   return (
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,7 @@
-export const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-type Pokemon = (typeof pokemonList)[number];
+type Pokemon = {
+    name: string;
+    imgSrc?: string;
+};
 
 function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
     return (
