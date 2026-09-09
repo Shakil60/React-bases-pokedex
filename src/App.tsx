007af2e3cc9135bda128e1f81587e39f1228a591
Shakil60 <khaldishakil@gmail.com>
2026-09-09 15:52:06 +0200

    génération dynamique des boutons pokémon avec map

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,21 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
@@ -26,7 +41,11 @@ function App() {
   return (
     <div>
       <nav>
-        {/* plus de boutons en dur ! */}
+        {pokemonList.map((pokemon) => (
+          <button key={pokemon.name} onClick={() => setPokemonName(pokemon.name)}>
+            {pokemon.name}
+          </button>
+        ))}
       </nav>
       <PokemonCard pokemon={pokemon} />
     </div>
